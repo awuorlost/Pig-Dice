a2d7eea4435632d6740f2c9f070f9fc6e04c84f4
Awuor <awuorattyang@gmail.com>
2018-04-12 23:52:58 -0700

    add back-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     $(".player-console").show();
     $(".start").hide();
 
-    var player1Name = $(".player1Name").val();
+    var player1Name = $(".player1name").val();
     $("#player1Name").text(player1Name);
 
     var player2Name = $(".player2Name").val();
@@ -62,3 +62,54 @@ $(document).ready(function() {
   });
 
 });
+
+//business logic
+var player1="";
+var player2="";
+
+var throwdice = function () {
+  return Math.floor(6*Math.random())+1;
+}
+
+function Player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  //this.playerName;
+}
+
+Player.prototype.rollone = function() {
+  if (this.roll === 1) {
+  this.tempscore = 0;
+  alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!")
+  } else {
+  this.tempscore += this.roll;
+  }
+}
+
+Player.prototype.hold = function () {
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  alert(this.playerName + ", your turn is over, pass the mouse!");
+}
+
+
+Player.prototype.winnerCheck = function () {
+  if (this.totalscore >= 10) {
+    alert(this.playerName + " You are the winner!");
+  }
+}
+
+Player.prototype.newGame = function () {
+
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.playerName ="";
+}
+
+var clearValues = function(){
+  $(".player1Name").val("");
+  $(".player2Name").val("");
+}
